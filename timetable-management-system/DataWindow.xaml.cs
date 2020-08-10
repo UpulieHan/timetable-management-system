cdf52f8db294049c872d6ae667daf5385b8bfef9
Upulie Han <ireshaupulie@gmail.com>
2020-08-10 15:02:57 +0530

    Testing CI thing for the second time.

--- timetable-management-system/DataWindow.xaml.cs
+++ timetable-management-system/DataWindow.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,25 +8,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace timetable_management_system
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for DataWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class DataWindow : Window
     {
-        public MainWindow()
+        public DataWindow()
         {
             InitializeComponent();
-        }
-        private void OpenWindow(object sender, RoutedEventArgs e)
-        {
-            DataWindow dataWindow = new DataWindow();
-            this.Visibility = Visibility.Hidden;//hiding the current window
-            dataWindow.Show();
         }
     }
 }
