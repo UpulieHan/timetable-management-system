b2e4d2170f6a90c9caec8f3e36bea347fce1aacb
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-08-10 22:11:23 +0530

    TestModelDbConfig

--- timetable-management-system/Models/TimetableDbContext.cs
+++ timetable-management-system/Models/TimetableDbContext.cs
@@ -8,10 +8,23 @@ namespace timetable_management_system.Models
 {
     class TimetableDbContext : DbContext
     {
+        public DbSet<Test> Tests { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(DbConfig.getConnectionString());
             base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Test>(entity =>
+            {
+                entity.HasKey(e => e.TestId);
+                entity.Property(e => e.Text);
+            });
         }
     }
 }
