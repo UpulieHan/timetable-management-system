1e5cbce1a816cd28c0ecfeefa30f2106ebd9618b
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-08-10 22:05:13 +0530

    db config setup

---
+++ timetable-management-system/Models/TimetableDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using timetable_management_system.Config;
+
+namespace timetable_management_system.Models
+{
+    class TimetableDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(DbConfig.getConnectionString());
+        }
+    }
+}
