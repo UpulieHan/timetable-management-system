5d88b1971b2ccf483cc4a2fa1e5474be102d5c38
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-08-11 10:37:08 +0530

    :rotating_light: :rotating_light: :rotating_light: This commit should be rebased before making the Repo Public. Added connection string. :rotating_light: :rotating_light: :rotating_light:

--- timetable-management-system/Config/DbConfig.cs
+++ timetable-management-system/Config/DbConfig.cs
@@ -8,7 +8,7 @@ namespace timetable_management_system.Config
     {
         public static string getConnectionString()
         {
-            string connectionString = "";
+            string connectionString = "Server=database-1.cmxg05ztpr3u.us-east-1.rds.amazonaws.com,1433;Database=timetableDb;User Id=admin;Password=test1234;";
 
             return connectionString;
         }
