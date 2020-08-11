b8d9036231ab37d081c9336119b31d1df940222d
Upulie Han <ireshaupulie@gmail.com>
2020-08-11 21:09:46 +0530

    Added the first menu page MainDataWindow and it's specific tabs. Tabs need more designing.

---
+++ timetable-management-system/View/DataWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace timetable_management_system.View
+{
+    /// <summary>
+    /// Interaction logic for DataWindow.xaml
+    /// </summary>
+    public partial class DataWindow : Window
+    {
+        public DataWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
