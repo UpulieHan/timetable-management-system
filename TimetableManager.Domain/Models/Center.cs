40e15bf18e480650a6039c76349879a08494eee3
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-08-12 02:29:47 +0530

    added TimetableManger.Domain project

---
+++ TimetableManager.Domain/Models/Center.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimetableManager.Domain.Models
+{
+    public class Center : DomainObject
+    {
+        public string CenterName { get; set; }
+    }
+}
---
+++ TimetableManager.Domain/Models/Faculty.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimetableManager.Domain.Models
+{
+    public class Faculty : DomainObject
+    {
+        public string FacultyName { get; set; }
+    }
+}
