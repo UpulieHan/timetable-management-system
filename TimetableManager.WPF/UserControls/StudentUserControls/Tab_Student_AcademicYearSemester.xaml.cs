c28cae51657fbdd6f875e2c3b67f7e1ec8ac93f4
TereenPrasanga <tereen1997@gmail.com>
2020-08-12 12:09:21 +0530

    Short name genarate using dropdowns

--- TimetableManager.WPF/UserControls/StudentUserControls/Tab_Student_AcademicYearSemester.xaml.cs
+++ TimetableManager.WPF/UserControls/StudentUserControls/Tab_Student_AcademicYearSemester.xaml.cs
@@ -18,6 +18,10 @@ namespace TimetableManager.WPF.UserControls.StudentUserControls
     /// </summary>
     public partial class Tab_Student_AcademicYearSemester : UserControl
     {
+        string Year;
+        string Semester;
+        string YearShortname;
+        string SemesterShortname;
         public Tab_Student_AcademicYearSemester()
         {
             InitializeComponent();
@@ -25,10 +29,52 @@ namespace TimetableManager.WPF.UserControls.StudentUserControls
 
         private void comboBoxYear_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            int selectedIndex = comboBoxYear.SelectedIndex;
             Object selectedItem = comboBoxYear.SelectedValue;
-            textBox.Text = selectedItem.ToString();
+            Year = selectedItem.ToString();
+            if(Year.Equals("Year 1"))
+            {
+                YearShortname = "Y1";
+            }
+            if (Year.Equals("Year 2"))
+            {
+                YearShortname = "Y2";
+            }
+            if (Year.Equals("Year 3"))
+            {
+                YearShortname = "Y3";
+            }
+            if (Year.Equals("Year 4"))
+            {
+                YearShortname = "Y4";
+            }
+            textBox.Text = YearShortname + SemesterShortname;
+
 
         }
+
+        private void comboBoxSemester_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Object selectedItem = comboBoxSemester.SelectedValue;
+            Semester = selectedItem.ToString();
+            if (Semester.Equals("Semester 1"))
+            {
+                SemesterShortname = "S1";
+            }
+            if (Semester.Equals("Semester 2"))
+            {
+                SemesterShortname = "S2";
+            }
+            if (Semester.Equals("Semester 3"))
+            {
+                SemesterShortname = "S3";
+            }
+            if (Semester.Equals("Semester 4"))
+            {
+                SemesterShortname = "S4";
+            }
+            textBox.Text = YearShortname + SemesterShortname;
+
+        }
+
     }
 }
