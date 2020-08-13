f2549233ecb95746d8e06c727b5229eec0143f88
Upulie Han <ireshaupulie@gmail.com>
2020-08-13 18:36:44 +0530

    Added the Cancel_tick button and did some partial validations for the inputs so the tick button would work. Almost done with Tab_Main_Days

--- TimetableManager.WPF/UserControls/DataViewControls/Tab_Main_Days.xaml.cs
+++ TimetableManager.WPF/UserControls/DataViewControls/Tab_Main_Days.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -18,32 +20,115 @@ namespace TimetableManager.WPF.Controls
     /// </summary>
     public partial class Tab_Main_Days : UserControl
     {
+        public ObservableCollection<BoolStringClass> TheDaysList { get; set; }
+        public int noOfDays = 0;
+        public int selectedNoOfDays;
+        public int? hours;
+        public int? mins;
+        public int? timeSlot;
         public Tab_Main_Days()
         {
             InitializeComponent();
+
+            //setting the selected Days list
+            TheDaysList = new ObservableCollection<BoolStringClass>();
+            TheDaysList.Add(new BoolStringClass { IsSelected = false, TheText = "Monday" });
+            TheDaysList.Add(new BoolStringClass { IsSelected = false, TheText = "Tuesday" });
+            TheDaysList.Add(new BoolStringClass { IsSelected = false, TheText = "Wednesday" });
+            TheDaysList.Add(new BoolStringClass { IsSelected = false, TheText = "Thursday" });
+            TheDaysList.Add(new BoolStringClass { IsSelected = false, TheText = "Friday" });
+            TheDaysList.Add(new BoolStringClass { IsSelected = false, TheText = "Saturday" });
+            TheDaysList.Add(new BoolStringClass { IsSelected = false, TheText = "Sunday" });
+
+            this.DataContext = this;
         }
 
         private void comboBoxNoOfDays_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            //int selectedIndex = comboBoxNoOfDays.SelectedIndex;
-            //Object selectedItem = comboBoxNoOfDays.SelectedValue;
-            //textBox.Text = selectedItem.ToString();
-
+            string text = (e.AddedItems[0] as ComboBoxItem).Content.ToString();
+            noOfDays = Int32.Parse(text);
         }
+
         private void comboBoxHours_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            //int selectedIndex = comboBoxNoOfDays.SelectedIndex;
-            //Object selectedItem = comboBoxNoOfDays.SelectedValue;
-            //textBox.Text = selectedItem.ToString();
-
+            string text = (e.AddedItems[0] as ComboBoxItem).Content.ToString();
+            hours = Int32.Parse(text);
         }
         private void comboBoxMinutes_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            //int selectedIndex = comboBoxNoOfDays.SelectedIndex;
-            //Object selectedItem = comboBoxNoOfDays.SelectedValue;
-            //textBox.Text = selectedItem.ToString();
+            string text = (e.AddedItems[0] as ComboBoxItem).Content.ToString();
+            mins = Int32.Parse(text);
+        }
+
+        private void comboBoxDuration_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string text = (e.AddedItems[0] as ComboBoxItem).Content.ToString();
+
+            if (text.Equals("30 mins"))
+            {
+                timeSlot = 30;
+            }
+            else if (text.Equals("1 hour"))
+            {
+                timeSlot = 60;
+            }
+        }
+
+        private void mouseEnter_Event(object sender, EventArgs e)
+        {
+            checkDateValidity();
+        }
+
+        //
+        private void saveButton_Click(object sender, RoutedEventArgs e)
+        {
+            bool ret = checkDateValidity();
+
+            if (ret && (hours != null) && (hours != 0) && (mins != null) && (timeSlot != null) && (timeSlot != 0))
+            {
+                var uriSource = new Uri("/Resources/Save_tick.png", UriKind.Relative);
+                tickImage.Source = new BitmapImage(uriSource);
+                MessageBox.Show("All good");
+            }
+            else
+            {
+                var uriSource = new Uri("/Resources/Cancel_tick.png", UriKind.Relative);
+                tickImage.Source = new BitmapImage(uriSource);
+                MessageBox.Show("Not all values match. Please try again");
+            }
 
         }
 
+        private bool checkDateValidity()
+        {
+            selectedNoOfDays = 0;
+
+            //the selected no of days days
+            foreach (var item in TheDaysList)
+            {
+
+                if (item.IsSelected == true)
+                {
+                    Trace.WriteLine(item.TheText);
+                    Trace.WriteLine("Now selected no of days is " + item.TheText);
+                    selectedNoOfDays++;
+                }
+            }
+
+            //the number of days
+            if (selectedNoOfDays == noOfDays)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public class BoolStringClass
+        {
+            public string TheText { get; set; }
+            public bool IsSelected { get; set; }
+        }
     }
 }
