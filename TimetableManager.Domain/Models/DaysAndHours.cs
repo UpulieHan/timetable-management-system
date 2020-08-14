0f3a6b22d70e87e55d2d3d707d2514b9693c2f36
Upulie Han <ireshaupulie@gmail.com>
2020-08-14 15:55:41 +0530

    Added the Day and DaysAndHours Tables. Removed the DayList on Tab_Main_Days to retrieve it from the DB (can't retrieve Days values from DB yet).

---
+++ TimetableManager.Domain/Models/DaysAndHours.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace TimetableManager.Domain.Models
+{
+    public class DaysAndHours
+    {
+        public int Id { get; set; }
+        public int NoOfDays { get; set; }
+
+        //how to reference this data on the xaml?
+        public ObservableCollection<Day> DayList { get; set; }
+        public int Hours { get; set; }
+        public int Mins { get; set; }
+        public int TimeSlot { get; set; }
+    }
+}
