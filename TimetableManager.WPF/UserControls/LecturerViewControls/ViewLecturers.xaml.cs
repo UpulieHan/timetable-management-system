0c88034e46d7295227b3d67b69f5747858bba662
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-08-19 23:05:57 +0530

    basic data view added

--- TimetableManager.WPF/UserControls/LecturerViewControls/ViewLecturers.xaml.cs
+++ TimetableManager.WPF/UserControls/LecturerViewControls/ViewLecturers.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TimetableManager.Domain.Models;
+using TimetableManager.EntityFramework.Services;
 
 namespace TimetableManager.WPF.UserControls.LecturerViewControls
 {
@@ -18,9 +22,38 @@ namespace TimetableManager.WPF.UserControls.LecturerViewControls
     /// </summary>
     public partial class ViewLecturers : UserControl
     {
+        public ObservableCollection<LecturerGridModel> LecturerDataList { get; private set; }
+        public List<Lecturer> LecturerList { get; private set; }
         public ViewLecturers()
         {
             InitializeComponent();
+            this.DataContext = this;
+
+            LecturerDataList = new ObservableCollection<LecturerGridModel>();
+
+            _ = this.LoadLecturerData();
+        }
+
+        private async Task LoadLecturerData()
+        {
+            LecturerDataService lecturerDataService = new LecturerDataService(new EntityFramework.TimetableManagerDbContext());
+
+            LecturerList = await lecturerDataService.GetLecturersAsync();
+
+            LecturerList.ForEach(e =>
+            {
+                LecturerGridModel lecturerObj = new LecturerGridModel();
+                lecturerObj.EmployeeId = e.EmployeeId;
+                lecturerObj.EmployeeName = e.EmployeeName;
+                lecturerObj.FacultyName = e.Faculty.FacultyName;
+                lecturerObj.DepartmentName = e.Department.DepartmentName;
+                lecturerObj.CenterName = e.Center.CenterName;
+                lecturerObj.BuildingName = e.Building.BuildingName;
+                lecturerObj.LevelName = e.Level.LevelName;
+                lecturerObj.Rank = e.Rank;
+
+                LecturerDataList.Add(lecturerObj);
+            });
         }
     }
 }
