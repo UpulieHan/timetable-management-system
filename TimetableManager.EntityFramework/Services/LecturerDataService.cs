bf81cc65792b1eae3c3f677c2f13641680698f7e
Amith Mihiranga <39789194+lwpamihiranga@users.noreply.github.com>
2020-09-03 16:10:30 +0530

    Refactor Lecturer Add/Edit/View Part 2 - Data Service (#49)
    * changed to use sqlite db
    
    * delete older user controls
    
    * implement the login inside tab control
    
    * lecture data service refactor

--- TimetableManager.EntityFramework/Services/LecturerDataService.cs
+++ TimetableManager.EntityFramework/Services/LecturerDataService.cs
@@ -21,11 +21,18 @@ namespace TimetableManager.EntityFramework.Services
 
         public Task<int> AddLecturer(Lecturer lecturer, string fName, string dName, string cName, string bName, string lName)
         {
+            SetLectureFields(lecturer, fName, dName, cName, bName, lName);
+
+            return _context.SaveChangesAsync();
+        }
+
+        private void SetLectureFields(Lecturer lecturer, string fName, string dName, string cName, string bName, string lName)
+        {
             var faculty = _context.Faculties.Include(e => e.Lecturers).Single(e => e.FacultyName == fName);
             faculty.Lecturers.Add(lecturer);
 
             var department = _context.Departments.Include(e => e.Lecturers).Single(e => e.DepartmentName == dName);
-            department .Lecturers.Add(lecturer);
+            department.Lecturers.Add(lecturer);
 
             var center = _context.Centers.Include(e => e.Lecturers).Single(e => e.CenterName == cName);
             center.Lecturers.Add(lecturer);
@@ -35,8 +42,6 @@ namespace TimetableManager.EntityFramework.Services
 
             var level = _context.Levels.Include(e => e.Lecturers).Single(e => e.LevelName == lName);
             level.Lecturers.Add(lecturer);
-
-            return _context.SaveChangesAsync();
         }
 
         public async Task<List<Lecturer>> GetLecturersAsync()
@@ -74,20 +79,7 @@ namespace TimetableManager.EntityFramework.Services
         {
             await this.DeleteLecturer(lecturer.EmployeeId);
 
-            var faculty = _context.Faculties.Include(e => e.Lecturers).Single(e => e.FacultyName == fName);
-            faculty.Lecturers.Add(lecturer);
-
-            var department = _context.Departments.Include(e => e.Lecturers).Single(e => e.DepartmentName == dName);
-            department.Lecturers.Add(lecturer);
-
-            var center = _context.Centers.Include(e => e.Lecturers).Single(e => e.CenterName == cName);
-            center.Lecturers.Add(lecturer);
-
-            var building = _context.Buildings.Include(e => e.Lecturers).Single(e => e.BuildingName == bName);
-            building.Lecturers.Add(lecturer);
-
-            var level = _context.Levels.Include(e => e.Lecturers).Single(e => e.LevelName == lName);
-            level.Lecturers.Add(lecturer);
+            SetLectureFields(lecturer, fName, dName, cName, bName, lName);
 
             return await _context.SaveChangesAsync();
         }
