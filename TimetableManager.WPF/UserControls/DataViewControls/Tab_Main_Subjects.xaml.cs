69b79557776264d1c477ed0554b8c7eecf468a4f
Amith Mihiranga <39789194+lwpamihiranga@users.noreply.github.com>
2020-09-05 00:20:21 +0530

    Refactor 1 Part 9 (#56)
    * locations refactored
    
    * remove old user controls after refactor
    
    * completed year semester updata/delele/add refactor
    
    * subject implementation part 1
    
    * refactored migrations v1
    
    * subject delete method added
    
    * empty fields checking
    
    * stop duplication generations
    
    * check for number input in subject add
    
    * remove refactor migrations

--- TimetableManager.WPF/UserControls/DataViewControls/Tab_Main_Subjects.xaml.cs
+++ TimetableManager.WPF/UserControls/DataViewControls/Tab_Main_Subjects.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -160,6 +161,12 @@ namespace TimetableManager.WPF.Controls
             });
 
             _ = SubjectDataList.Remove(selectedSubject);
+        }
+
+        private void CheckTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
         }
     }
 }
