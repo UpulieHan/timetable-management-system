220db1a8ee87780a2b6395038104ab63cc156818
TereenPrasanga <tereen1997@gmail.com>
2020-09-23 00:04:44 +0530

    member 1 stage

--- TimetableManager.WPF/Views/MainAvailabilityWindow.xaml.cs
+++ TimetableManager.WPF/Views/MainAvailabilityWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -9,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using TimetableManager.Domain.Models;
+using TimetableManager.EntityFramework.Services;
 
 namespace TimetableManager.WPF.Views
 {
@@ -17,9 +21,84 @@ namespace TimetableManager.WPF.Views
     /// </summary>
     public partial class MainAvailabilityWindow : Window
     {
+        public string val;
+        public List<Lecturer> LecturersList { get;  set; }
+        public List<GroupId> GroupIdList { get;  set; }
+        public List<SubGroupId> SubGroupIdList { get;  set; }
+
+        public ObservableCollection<string> NameList { get;  set; }
+
+
         public MainAvailabilityWindow()
         {
             InitializeComponent();
+            NameList = new ObservableCollection<string>();
+            this.DataContext = this;
+
         }
+
+        private void comboBoxSelectRes_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Object selectedItem = comboBoxSelectRes.SelectedValue;
+            val = selectedItem.ToString();
+            if (val.Equals("lecturer"))
+            {
+                _ = this.SetLectures();
+
+            }
+            else if (val.Equals("session"))
+            {
+
+            }
+            else if (val.Equals("group"))
+            {
+                _ = this.SetGroupId();
+
+            }
+            else
+            {
+                _ = this.SetSubGroupId();
+
+            }
+        }
+        private async Task SetGroupId()
+        {
+            GroupIdDataService groupIdDataService = new GroupIdDataService(new EntityFramework.TimetableManagerDbContext());
+
+            GroupIdList = await groupIdDataService.GetGroupId();
+
+            GroupIdList.ForEach(list =>
+            {
+                NameList.Add(list.GroupID);
+
+            });
+        }
+        private async Task SetSubGroupId()
+        {
+            SubGroupIdDataService subGroupIdDataService = new SubGroupIdDataService(new EntityFramework.TimetableManagerDbContext());
+
+            SubGroupIdList = await subGroupIdDataService.GetSubGroupId();
+
+            SubGroupIdList.ForEach(list =>
+            {
+                NameList.Add(list.SubGroupID);
+
+            });
+        }
+        private async Task SetLectures()
+        {
+
+            LecturerDataService lecturerDataService = new LecturerDataService(new EntityFramework.TimetableManagerDbContext());
+
+            LecturersList = await lecturerDataService.GetLecturersAsync();
+
+            LecturersList.ForEach(list =>
+            {
+                NameList.Add(list.EmployeeName);
+            });
+        }
+      
+
+
     }
 }
