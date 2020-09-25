4b03b0ab817b5ab7f2997a5406d4f2b0f3aaf098
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-09-25 16:33:39 +0530

    update session data service

--- TimetableManager.EntityFramework/Services/SessionDataService.cs
+++ TimetableManager.EntityFramework/Services/SessionDataService.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TimetableManager.Domain.Models;
@@ -15,8 +17,9 @@ namespace TimetableManager.EntityFramework.Services
             _context = context;
         }
 
-        public Task<int> AddSession(Session session)
+        public Task<int> AddSession(Session session, List<Lecturer> lecturers, List<GroupId> groups)
         {
+            _context.Sessions.Add(session);
             return _context.SaveChangesAsync();
         }
     }
