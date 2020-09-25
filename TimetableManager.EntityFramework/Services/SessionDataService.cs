66a95fb01a61ac40168eaae89863c5f04ea1a4e9
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-09-25 14:29:23 +0530

    session data service added

---
+++ TimetableManager.EntityFramework/Services/SessionDataService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TimetableManager.Domain.Models;
+
+namespace TimetableManager.EntityFramework.Services
+{
+    public class SessionDataService
+    {
+        private readonly TimetableManagerDbContext _context;
+
+        public SessionDataService(TimetableManagerDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<int> AddSession(Session session)
+        {
+            return _context.SaveChangesAsync();
+        }
+    }
+}
