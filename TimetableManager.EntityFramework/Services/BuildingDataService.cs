ff6800bd6bfe317293ec21adc91d08b75f87134d
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-09-29 21:04:36 +0530

    remove unused imports in services

--- TimetableManager.EntityFramework/Services/BuildingDataService.cs
+++ TimetableManager.EntityFramework/Services/BuildingDataService.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using TimetableManager.Domain.Models;
 
@@ -11,7 +9,6 @@ namespace TimetableManager.EntityFramework.Services
     public class BuildingDataService
     {
         private readonly TimetableManagerDbContext _context;
-
 
         public BuildingDataService(TimetableManagerDbContext context)
         {
--- TimetableManager.EntityFramework/Services/LecturerDataService.cs
+++ TimetableManager.EntityFramework/Services/LecturerDataService.cs
@@ -1,10 +1,6 @@
-﻿using Microsoft.Data.SqlClient;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.VisualBasic;
-using System;
+﻿using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using TimetableManager.Domain.Models;
 
