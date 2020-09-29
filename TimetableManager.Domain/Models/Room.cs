63a5c3d95811fc98eb69d87fcce0be02d64a06be
Amith Mihiranga <lwpamihiranga@gmail.com>
2020-09-29 20:58:11 +0530

    remove unused imports in models

--- TimetableManager.Domain/Models/Room.cs
+++ TimetableManager.Domain/Models/Room.cs
@@ -1,19 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace TimetableManager.Domain.Models
+﻿namespace TimetableManager.Domain.Models
 {
     public class Room
     {
         public int RoomId { get; set; }
-
         public string RoomName { get; set; }
-
         public int Capacity { get; set; }
-
         public Building Building { get; set; }
-
         public Center Center { get; set; }
     }
 }
