92df4fc442ca2e502485efe3875971a010c6f072
Upulie Han <ireshaupulie@gmail.com>
2020-10-01 12:37:06 +0530

    resolving an error

--- TimetableManager.WPF/UserControls/StatisticsTimetableDataControls/TimetableUserControl/Tab_Timetable_Generate.xaml.cs
+++ TimetableManager.WPF/UserControls/StatisticsTimetableDataControls/TimetableUserControl/Tab_Timetable_Generate.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Text;
 using System.Windows;
@@ -11,11 +12,20 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TimetableManager.Domain.Models;
+using TimetableManager.EntityFramework;
 
 namespace TimetableManager.WPF.StatisticsTimetableDataControls.TimetableUserControl
 {
     public partial class Tab_Timetable_Generate : UserControl
     {
+        private TimetableManagerDbContext timetableManagerDbContext = new TimetableManagerDbContext();
+        private ObservableCollection<Day> theDaysList { get; set; }
+        private ObservableCollection<Session> theSessionsList { get; set; }
+        private ObservableCollection<GroupId> theGroupList { get; set; }
+        private ObservableCollection<SubGroupId> theSubGroupList { get; set; }
+        private ObservableCollection<Room> theRoomList { get; set; }
+
         public Tab_Timetable_Generate()
         {
             InitializeComponent();
@@ -23,6 +33,7 @@ namespace TimetableManager.WPF.StatisticsTimetableDataControls.TimetableUserCont
 
         private void generateButton_Click(object sender, RoutedEventArgs e)
         {
+            //set the title, cols (days), rows(timeslots)
             Trace.WriteLine("generateButton click");
         }
     }
--- TimetableManager.WPF/UserControls/StatisticsTimetableDataControls/TimetableUserControl/Tab_Timetable_Search.xaml.cs
+++ TimetableManager.WPF/UserControls/StatisticsTimetableDataControls/TimetableUserControl/Tab_Timetable_Search.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Text;
 using System.Windows;
@@ -11,24 +12,83 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TimetableManager.Domain.Models;
+using TimetableManager.EntityFramework;
 using TimetableManager.WPF.Views;
 
 namespace TimetableManager.WPF.StatisticsTimetableDataControls.TimetableUserControl
 {
     public partial class Tab_Timetable_Search : UserControl
     {
+        public ObservableCollection<string> comboBoxValueList;
+        private ObservableCollection<Lecturer> theLecturerList { get; set; }
+        private ObservableCollection<GroupId> theGroupList { get; set; }
+        private ObservableCollection<SubGroupId> theSubGroupList { get; set; }
+        private ObservableCollection<Room> theRoomList { get; set; }
+
+        private TimetableManagerDbContext timetableManagerDbContext = new TimetableManagerDbContext();
         public Tab_Timetable_Search()
         {
             InitializeComponent();
+            comboBoxValueList = new ObservableCollection<string>();
+
+            try
+            {
+                theLecturerList = new ObservableCollection<Lecturer>(timetableManagerDbContext.Lecturers);
+                theGroupList = new ObservableCollection<GroupId>(timetableManagerDbContext.GroupIds);
+                theSubGroupList = new ObservableCollection<SubGroupId>(timetableManagerDbContext.SubGroupIds);
+                theRoomList = new ObservableCollection<Room>(timetableManagerDbContext.Rooms);
+            }
+            catch (Exception e)
+            {
+                //in case if the connection to the DB is lost
+                MessageBox.Show("first error " + e.Message);
+            }
+            this.DataContext = this;
+
         }
 
         private void comboBoxTimetableType_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            Trace.WriteLine("comboBoxTimetableType clicked");
+            string selectedValue = (e.AddedItems[0] as ComboBoxItem).Content.ToString();
+
+            //set comboBocValueList accordingly
+            if (selectedValue == "Lecturer")
+            {
+                comboBoxValueList.Clear();
+                foreach (Lecturer l in theLecturerList)
+                {
+                    comboBoxValueList.Add(l.EmployeeName);
+                }
+            }
+            else if (selectedValue == "Group")
+            {
+                comboBoxValueList.Clear();
+                foreach (GroupId g in theGroupList)
+                {
+                    comboBoxValueList.Add(g.GroupID);
+                }
+                foreach (SubGroupId s in theSubGroupList)
+                {
+                    comboBoxValueList.Add(s.SubGroupID);
+                }
+            }
+            else if (selectedValue == "Room")
+            {
+                comboBoxValueList.Clear();
+                foreach (Room r in theRoomList)
+                {
+                    comboBoxValueList.Add(r.RoomName);
+                }
+            }
+
+            comboBoxSpecificName.ItemsSource = comboBoxValueList;
+            comboBoxSpecificName.Items.Refresh();
         }
         private void comboBoxSpecificName_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            Trace.WriteLine("comboBoxSpecificName clicked");
+            string selectedValue = (e.AddedItems[0] as ComboBoxItem).Content.ToString();
+            Trace.WriteLine(selectedValue);
         }
 
 
