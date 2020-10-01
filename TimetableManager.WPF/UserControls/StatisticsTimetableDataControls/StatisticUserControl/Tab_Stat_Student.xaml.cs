477dcbcefb2a338dfbe94227a2a68b8d6e417e85
HusmithaSilva <53034252+HusmithaSilva@users.noreply.github.com>
2020-10-01 19:57:26 +0530

    Stat std subj (#76)
    * commit-statistics
    
    * commit-subject-stat

--- TimetableManager.WPF/UserControls/StatisticsTimetableDataControls/StatisticUserControl/Tab_Stat_Student.xaml.cs
+++ TimetableManager.WPF/UserControls/StatisticsTimetableDataControls/StatisticUserControl/Tab_Stat_Student.xaml.cs
@@ -1,6 +1,23 @@
-﻿using System.Collections.Generic;
+
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TimetableManager.Domain.Models;
+using TimetableManager.EntityFramework.Services;
+
+
+
 
 namespace TimetableManager.WPF.StatisticsTimetableDataControls.StatisticUserControl
 {
@@ -9,20 +26,21 @@ namespace TimetableManager.WPF.StatisticsTimetableDataControls.StatisticUserCont
     /// </summary>
     public partial class Tab_Stat_Student : UserControl
     {
+        
+        public List<GroupId> GrpIdList { get; private set; }
+        public List<GroupNumber> GrpNumberList { get; private set; }
+
+        public Hashtable HashTable = new Hashtable();
+        public List<StdStatGrid> StudentStatList = new List<StdStatGrid>();
+
+
+
         public Tab_Stat_Student()
         {
             InitializeComponent();
-            List<Std> std = new List<Std>();
-
-            std.Add(new Std() { programme="common", year = "1st year", subgroups = 8 });
-            std.Add(new Std() { programme = "common",  year = "2st year", subgroups = 7 });
-            std.Add(new Std() { programme = "SE", year = "3st year", subgroups = 3 });
-            std.Add(new Std() { programme = "SE", year = "4st year", subgroups = 2 });
-            std.Add(new Std() { programme = "ISE", year = "3st year", subgroups = 2 });
-            std.Add(new Std() { programme = "ISE", year = "4st year", subgroups = 1 });
-            std.Add(new Std() { programme = "IT", year = "3st year", subgroups = 3 });
-            std.Add(new Std() { programme = "IT", year = "4st year", subgroups = 2 });
-            dataGridstd.ItemsSource = std;
+          
+            _ = this.LoadProgramme();
+            dataGridstd.ItemsSource = StudentStatList;
 
         }
 
@@ -30,14 +48,57 @@ namespace TimetableManager.WPF.StatisticsTimetableDataControls.StatisticUserCont
         {
 
         }
+
+        private async Task LoadProgramme()
+        {
+            GroupIdDataService programmeDataService = new GroupIdDataService(new EntityFramework.TimetableManagerDbContext());
+            GrpIdList = await programmeDataService.GetGroupId();
+
+            int sumY1 = 0;
+            int sumY2 = 0;
+            int sumY3 = 0;
+            int sumY4 = 0;
+
+
+            GrpIdList.ForEach(g =>
+            {
+
+                if (g.GroupID.StartsWith("Y1"))
+                {
+                  
+                    sumY1 = sumY1 + 1;
+                   
+                } if (g.GroupID.StartsWith("Y2"))
+                {
+                    
+                    sumY2 = sumY2 + 1;
+                } if(g.GroupID.StartsWith("Y3"))
+                {
+                    sumY3 = sumY3 + 1;
+                }
+                if(g.GroupID.StartsWith("Y4")){
+                    sumY4 = sumY4 + 1;
+                }
+
+            
+            });
+
+            StudentStatList.Add(new StdStatGrid { Year1 = sumY1 , Year2 = sumY2, Year3 = sumY3, Year4 = sumY4 });
+
+        }
+
+
     }
 
-    internal class Std
+    public class StdStatGrid
     {
 
-        public string programme { get; set; }
-        public string year { get; set; }
+        public int Year1 { get; set; }
 
-        public int subgroups { get; set; }
+        public int Year2 { get; set; }
+
+        public int Year3 { get; set; }
+
+        public int Year4 { get; set; }
     }
 }
